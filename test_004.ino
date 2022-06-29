91858bdfd13eeeb4ceb60e6c465825d6635cce03
Ricky O'kawa <83910358+Rikiya89@users.noreply.github.com>
2022-06-29 21:26:15 +0900

    Update test_004.ino

--- test_004.ino
+++ test_004.ino
@@ -1,13 +1,25 @@
+// LEDのピン番号
+int LED = 13;
+char data;
+
 void setup() {
-  // put your setup code here, to run once:
+  // LEDピンを出力モードに設定
+  pinMode(LED, OUTPUT);
+  // シリアル通信の準備
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.print("Arduinoテスト");
-  Serial.println();
+  while (Serial.available()) {
+    data = Serial.read();
 
-  delay(1000);
+    if (data == '1') {
+      digitalWrite(LED, HIGH);
+    } else if (data == '2') {
+      digitalWrite(LED, LOW);
+    }
+  }
 
+  // 適度に間隔を開ける
+  delay(10);
 }
